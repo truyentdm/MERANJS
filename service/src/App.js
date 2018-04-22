6d17513d96cd1b380c3d8abfae82a8afb56c731f
TruyenTDM <truyen95@gmail.com>
2018-04-23 00:27:06 +0700

    Edit namespace IO

--- service/src/App.js
+++ service/src/App.js
@@ -2,7 +2,7 @@ import React,{Component} from "react"
 import ReactDOM from "react-dom"
 import io from 'socket.io-client';
 
-const socket = io("http://localhost:8080/my-namespace");
+const socket = io("/my-namespace");
 
 class App extends Component{
 	constructor() {
